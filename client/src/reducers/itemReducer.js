f7a06b76887400847832af55e33c9f68c8bbb620
robbie <robbiekruszynski@gmail.com>
2020-05-24 19:42:23 -0700

    add logic to reducer

---
+++ client/src/reducers/itemReducer.js
@@ -0,0 +1,24 @@
+import { v4 as uuid } from "uuid";
+import { GET_ITEMS, ADD_ITEM, DELETE_ITEM } from "../actions/types";
+
+const initalState = {
+  items: [
+    { id: uuid(), name: "Eggs" },
+    { id: uuid(), name: "Butter" },
+    { id: uuid(), name: "Chives" },
+    { id: uuid(), name: "Salt" },
+    { id: uuid(), name: "Pepper" },
+    { id: uuid(), name: "Cheese" },
+  ],
+};
+
+export default function (state = initialStat, action) {
+  switch (action.type) {
+    case GET_ITEMS:
+      return {
+        ...state,
+      };
+    default:
+      return state;
+  }
+}
